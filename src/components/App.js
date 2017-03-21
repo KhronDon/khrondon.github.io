3f9f5f774ff2f38d82a7b028dd770cfc898fdb69
Brubaker Wills <brubakerwills@gmail.com>
2017-03-21 17:39:11 -0400

    basic layout updates'

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-
 import chooChooImage from '../images/choo-choo.png'
 import triangle from '../images/triangle.png'
 
@@ -49,42 +48,42 @@ class App extends Component {
       <section className='myWork'>
         <h1>My Work</h1>
         <img className='tri' src={triangle} width='100vh' height='100vw' />
+        <section className='Pics'>
+          <ul>
+            <li>
+              <a href='http://choo-choo-todo.surge.sh/'> <img src={chooChooImage} /> </a>
+            </li>
+            <li>
+              <img src={'https://demonszone.com/wp-content/uploads/2014/04/Opus-Eponymous.jpg'} />
+            </li>
+            <li>
+              <img src={'http://www.progarchives.com/progressive_rock_discography_covers/4492/cover_1454131452009.jpg'} />
+            </li>
+          </ul>
+        </section>
       </section>
       <section className='blogs'>
         <h1>Blogs</h1>
         <img className='tri' src={triangle} width='100vh' height='100vw' />
         <section className='blogPosts'>
-          {/* <div className='React'>
+          <div className='React'>
             <h2>Hello, React </h2>
             <h4>December 6th, 2016 </h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas velit eos quo illo vitae ducimus rerum explicabo quasi libero laudantium impedit quod expedita, dolores tempora aliquam veritatis maxime dolorem, officia.</p>
+            <p>I’m still clueless, so let’s start with that. We’re only a couple weeks into learning JavaScript, and now it’s time to learn React. I don’t know much about it yet, but it seems like another mountain to climb. I’ve only had a small amount of exposure to React thus far, but I’m already starting to realize it’s potential... </p>
             <a className='More' href='https://medium.com/@brubakerwills/hello-react-9d50f7395181#.4k7blgpgm'>Keep Reading</a>
           </div>
           <div className='Fizz'>
-          <h2>FIZZ BUZZ </h2>
-          <h4>November 15th, 2016</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas velit eos quo illo vitae ducimus rerum explicabo quasi libero laudantium impedit quod expedita, dolores tempora aliquam veritatis maxime dolorem, officia.</p>
-          <a className='More' href='https://medium.com/@brubakerwills/fizzbuzz-666f0b37d5d5#.17ooucz7g'>Keep Reading</a>
-        </div> */}
-
+            <h2>FIZZ BUZZ </h2>
+            <h4>November 15th, 2016</h4>
+            <p>I’m just over a week in and I still feel clueless. So far my HTML and CSS have created webpages like neanderthal man cave drawings, except my buffalo look like giraffes and my fellow cavemen (and women) have too many limbs and oddly shaped heads. Now we’re moving on to JavaScript...</p>
+            <a className='More' href='https://medium.com/@brubakerwills/fizzbuzz-666f0b37d5d5#.17ooucz7g'>Keep Reading</a>
+          </div>
         </section>
-        <footer>Made with &hearts; in {this.state.location}</footer>
       </section>
-      <section className='contactMe' />
-      <section className='Pics'>
-        <ul>
-          <li>
-            <a href='http://choo-choo-todo.surge.sh/'> <img src={chooChooImage} /> </a>
-          </li>
-          <li>
-            <img src={'https://demonszone.com/wp-content/uploads/2014/04/Opus-Eponymous.jpg'} />
-          </li>
-          <li>
-            <img src={'http://www.progarchives.com/progressive_rock_discography_covers/4492/cover_1454131452009.jpg'} />
-          </li>
-        </ul>
+      <section className='contactMe'>
+        <h1>Contact Me</h1>
+        <img className='tri' src={triangle} width='100vh' height='100vw' />
       </section>
-
       <footer>Made with &hearts; in {this.state.location}</footer>
     </div>
   }
